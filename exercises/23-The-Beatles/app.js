4c09950520bea129ccd9236de8f35d646ec25d99
diegomarteens <diegomarteens@gmail.com>
2021-07-15 03:47:07 +0000

    23

--- exercises/23-The-Beatles/app.js
+++ exercises/23-The-Beatles/app.js
@@ -1,5 +1,20 @@
 
+function sing(){
+    let letItBe = 'let it be'
+    let whisper = 'whisper words of wisdom'
+    let answer = 'there will be an answer'
+    let array = [];
+    for(let i = 0; i<4; i++){
+        array.push(letItBe)
+    }
+    array.push(whisper);
 
-//Your code above ^^^
+    for(let i = 0; i<5; i++){
+        array.push(letItBe)
+    }
+        array.push(answer)
+        array.push(letItBe)
+        return array.join(', ')
+}
 
 console.log(sing());
