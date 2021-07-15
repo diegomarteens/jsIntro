8d31e4b7807274b436adc758c35027654bded557
diegomarteens <diegomarteens@gmail.com>
2021-07-15 02:00:40 +0000

    18

--- exercises/18-Create-a-for-loop/app.js
+++ exercises/18-Create-a-for-loop/app.js
@@ -1,7 +1,14 @@
 /* This exercise should be moved before 17-Your first loop? */
 
 // Declare and write your function here:
-
+function standardsMaker()
+{
+    for(var i = 0; i < 301; i++)
+    {
+        console.log("I will write questions if I'm Stuck");
+    }
+    return i;
+}
 
 // Calling the function:
 standardsMaker();
